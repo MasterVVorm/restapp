151728bf393123adacea04c97bb08608bf2c8509
Mark <mvincha404@gmail.com>
2019-12-10 18:18:40 +0300

    added build

---
+++ build/precache-manifest.da7fe6d125e6cd9c2035e1e567efd9e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb749cad671c9b502cd3df6f2cfa2ffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41807fc2ca956713134f",
+    "url": "/static/js/2.b22b813e.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/static/js/2.b22b813e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b65632a11bc3461650d",
+    "url": "/static/js/main.1f78cd3a.chunk.js"
+  },
+  {
+    "revision": "96610b5e87b6c1bc2690",
+    "url": "/static/js/runtime-main.7e2fc82f.js"
+  },
+  {
+    "revision": "5171f05200999d7dbac83658f8f1b491",
+    "url": "/static/media/Museo.5171f052.otf"
+  }
+]);
